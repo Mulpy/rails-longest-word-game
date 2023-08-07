c61c1b18b11e50aa4d4ab367ba2241875fc1f89b
Gary Topping <toppinggary@hotmail.com>
2023-08-07 13:44:50 +0900

    score added to letters games

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -3,27 +3,35 @@ class GamesController < ApplicationController
   require 'json'
 
   def new
-    @letters = Array.new(10) { ('A'..'Z').to_a.sample }
+  @start = Time.now()
+  @letters = Array.new(10) { ('A'..'Z').to_a.sample }
   end
 
   def score
-    @word = params[:word].upcase.chars
-    @letters = params[:letters].split(' ')
-    @included = @word.all? { |letter| @word.count(letter) <= @letters.count(letter) }
-    @url = "https://wagon-dictionary.herokuapp.com/#{params[:word]}"
-    @response = URI.parse(@url)
-    @json = JSON.parse(@response.read)
-    @found = @json["found"]
+    @time = params[:time]
     @score = 0
-    if @included
-      if @found
+    if self.included
+      if self.found
         @result = "Congratulations! #{params[:word].upcase} is a valid English word!"
-        @score = @word.size
+        @score = (@word.size * 10) / (@time.to_f * 1000)
       else
         @result = "Sorry, but #{params[:word].upcase} does not appear to be a valid English word!"
       end
     else
       @result = "Sorry but #{params[:word].upcase} can't be built out of #{params[:letters].split.join(', ')}"
     end
+  end
+
+  def included
+    @word = params[:word].upcase.chars
+    @letters = params[:letters].split(' ')
+    @word.all? { |letter| @word.count(letter) <= @letters.count(letter) }
+  end
+
+  def found
+    @url = "https://wagon-dictionary.herokuapp.com/#{params[:word]}"
+    @response = URI.parse(@url)
+    @json = JSON.parse(@response.read)
+    @json['found']
   end
 end
